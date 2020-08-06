e660078cab99464fcdb85c0cc190052f175fbae1
nlitvin <nlitvin@rendmate.com>
2020-08-06 17:55:35 +0300

    Features in progress.

---
+++ src/pages/Atm/components/screen/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import {PropTypes} from "./types";
+
+export default function (props: PropTypes){
+    return (
+        <div></div>
+    )
+}
---
+++ src/pages/Atm/components/screen/types.ts
@@ -0,0 +1,3 @@
+export interface PropTypes{
+    isValid: Promise<boolean>
+}
